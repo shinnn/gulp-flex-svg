7297e251ec595a3ae8a7d8d28ba09696f44cc970
shinnn <snnskwtnb@gmail.com>
2014-03-08 15:11:09 +0900

    use ES6 syntax

--- index-es6.js
+++ index-es6.js
@@ -1,26 +1,35 @@
 'use strict';
+
 var through = require('through2');
 var PluginError = require('gulp-util').PluginError;
 var flexSvg = require('flex-svg');
-var PLUGIN_NAME = 'gulp-flex-svg';
-module.exports = (function() {
+
+const PLUGIN_NAME = 'gulp-flex-svg';
+
+// Plugin level function (dealing with files)
+module.exports = () => {
+  
+  // Creating a stream through which each file will pass
   var stream = through.obj(function(file, enc, callback) {
-    var $__0 = this;
     if (file.isNull()) {
-      this.push(file);
+      this.push(file); // Do nothing if no contents
       return callback();
     }
+
     if (file.isBuffer()) {
-      flexSvg(file.contents, (function(err, result) {
+      flexSvg(file.contents, (err, result) => {
         file.contents = new Buffer(result);
-        $__0.push(file);
+        this.push(file);
         return callback();
-      }));
+      });
     }
+
     if (file.isStream()) {
       this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
       return callback();
     }
   });
+
+  // returning the file stream
   return stream;
-});
+};
