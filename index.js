fbf3b56c6f6b44a6894120fda9b1f2e82c6b6794
shinnn <snnskwtnb@gmail.com>
2014-04-05 03:07:10 +0900

    ignore ES5 files

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 'use strict';
+
 var through = require('through2');
-var PluginError = require('gulp-util').PluginError;
+var {PluginError} = require('gulp-util');
 var flexSvg = require('flex-svg');
-var PLUGIN_NAME = 'gulp-flex-svg';
-module.exports = (function() {
-  var stream = through.obj(function(file, enc, cb) {
-    var $__0 = this;
+
+const PLUGIN_NAME = 'gulp-flex-svg';
+
+// Plugin level function (dealing with files)
+module.exports = () => {
+  
+  // Creating a stream through which each file will pass
+  let stream = through.obj(function(file, enc, cb) {
     if (file.isNull()) {
-      this.push(file);
+      this.push(file); // Do nothing if no contents
       return cb();
     }
+
     if (file.isBuffer()) {
-      flexSvg(file.contents, (function(err, result) {
+      flexSvg(file.contents, (err, result) => {
         file.contents = new Buffer(result);
-        $__0.push(file);
+        this.push(file);
         return cb();
-      }));
+      });
     }
+
     if (file.isStream()) {
       return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
     }
   });
+
+  // returning the file stream
   return stream;
-});
+};
--- test.js
+++ test.js
@@ -1,20 +1,49 @@
 var assert = require('assert');
 var es = require('event-stream');
 var File = require('vinyl');
-var gulpFlexSvg = require('./index');
-var fixture = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"50px\" height=\"50px\">\n  <rect width=\"50px\" height=\"50px\"></rect>\n</svg>";
-var expected = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\">\n  <rect width=\"50px\" height=\"50px\"/>\n</svg>";
-describe('gulp-flex-svg', (function() {
-  describe('in buffer mode', (function() {
-    it('should remove width and height attributes', (function(done) {
-      var fakeFile = new File({contents: new Buffer(fixture)});
-      var myFlexSvg = gulpFlexSvg();
+
+let {main} = require('./package.json');
+var gulpFlexSvg = require(main);
+
+var fixture = `\
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px">
+  <rect width="50px" height="50px"></rect>
+</svg>`;
+
+var expected =`\
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+  <rect width="50px" height="50px"/>
+</svg>`;
+
+describe('gulp-flex-svg', () => {
+  describe('in buffer mode', () => {
+
+    it('should remove width and height attributes', (done) => {
+
+      // create the fake file
+      let fakeFile = new File({
+        contents: new Buffer(fixture)
+      });
+
+      // Create a prefixer plugin stream
+      let myFlexSvg = gulpFlexSvg();
+
+      // write the fake file to it
       myFlexSvg.write(fakeFile);
-      myFlexSvg.once('data', (function(file) {
+
+      // wait for the file to come back out
+      myFlexSvg.once('data', (file) => {
+        // make sure it came out the same way it went in
         assert(file.isBuffer());
+
+        // check the contents
         assert.equal(file.contents.toString('utf8'), expected);
         done();
-      }));
-    }));
-  }));
-}));
+      });
+
+    });
+
+  });
+});
