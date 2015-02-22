a3f7ec443f010cdda35a118b11e90fe78a4eb997
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-02-23 05:20:40 +0900

    introduce vinyl-bufferstream
    https://github.com/shinnn/vinyl-bufferstream

--- index.js
+++ index.js
@@ -4,53 +4,30 @@
 */
 'use strict';
 
-var BufferStreams = require('bufferstreams');
 var FlexSvg = require('flex-svg').FlexSvg;
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var through = require('through2');
+var VinylBufferStream = require('vinyl-bufferstream');
 
 module.exports = function gulpFlexSvg(options) {
   var flexSvg = new FlexSvg(options);
+  var run = new VinylBufferStream(function(buf, done) {
+    flexSvg(buf, function(err, result) {
+      if (err) {
+        done(err);
+        return;
+      }
+      done(null, new Buffer(result));
+    });
+  });
 
   return through.obj(function(file, enc, cb) {
-    if (file.isNull()) {
-      cb(null, file);
-      return;
-    }
-
     var self = this;
 
-    function run(buf, done) {
-      flexSvg(buf, function(err, result) {
-        if (err) {
-          err.fileName = file.path;
-          done(new PluginError('gulp-flex-svg', err));
-          return;
-        }
-        done(null, new Buffer(result));
-      });
-    }
-
-    if (file.isStream()) {
-      file.contents = file.contents.pipe(new BufferStreams(function(none, buf, done) {
-        run(buf, function(err, contents) {
-          if (err) {
-            self.emit('error', err);
-            done(err);
-          } else {
-            done(null, contents);
-            self.push(file);
-          }
-          cb();
-        });
-      }));
-      return;
-    }
-
-    run(file.contents, function(err, contents) {
+    run(file, function(err, contents) {
       if (err) {
-        self.emit('error', err);
+        self.emit('error', new PluginError('gulp-flex-svg', err, {fileName: file.path}));
       } else {
         file.contents = contents;
         self.push(file);
